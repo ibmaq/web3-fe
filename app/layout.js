b2e6583d2c88f93ec4ea9815153a5bd570b3d861
ibmaq <ibrahimaq99@gmail.com>
2025-06-17 23:31:06 +0500

    Refactor layout to remove unused Geist font and update font styling to use SF Pro Display

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import "./fonts.css";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -20,7 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} antialiased font-sf-pro`}
+        style={{ fontFamily: "'SF Pro Display', sans-serif" }}
       >
         {children}
       </body>
